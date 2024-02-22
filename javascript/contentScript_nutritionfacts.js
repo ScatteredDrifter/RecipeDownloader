ff7828bcf69c8e418826eb565a09deb42ec09a3d
Evelyn <contact.evelyn_stng@proton.me>
2024-02-23 00:47:32 +0100

    remove download and conversion from content script, improve control flow

--- javascript/contentScript_nutritionfacts.js
+++ javascript/contentScript_nutritionfacts.js
@@ -1,326 +1,171 @@
+// contentScript gathering data from selected website
+// returns formatted data to main.js
 
-var filePrefix =""
-var selectedFileType =""
+// TODO sentinel values
+var filePrefix = "";
+var selectedFileType = "";
 
 browser.runtime.onMessage.addListener((request) => {
-    console.log("gathering message")
-    console.log(request.filePrefix);
-    console.log(request.fileType);
-    filePrefix = request.filePrefix
-    selectedFileType = request.fileType
+  console.log("gathering message");
+  filePrefix = request.filePrefix;
+  selectedFileType = request.fileType;
+  var usedWebsite = request.usedWebsite;
 
-    //  executing download
-    executeDownload()
-})
+  //  executing download
+  var recipeData = queryNutritionfacts();
+  // returning obtained value to sender
+  return Promise.resolve({
+    recipe: recipeData,
+  });
+});
 
-// window.onload(console.log("script executed"),executeDownload());
+function queryNutritionfacts() {
+  console.log("querying nutritionfacts");
+  var recipeName =
+    document.getElementsByClassName("wprm-recipe-name")[0].innerHTML;
 
+  var descriptionList = gatherDescription();
+  var ingredientList = gatherIngredients();
+  var instructionList = gatherInstructions();
+  var recipeLink = window.location.href;
+  var portionAmount = gatherPortionAmount();
+  var imageSource = gatherImage();
 
-function executeDownload(){
-    
-    obtainedData = gatherContent()
+  condensedInformation = {
+    name: recipeName,
+    url: recipeLink,
+    portion: portionAmount,
+    imageSource: imageSource,
+    description: summarizeArray(descriptionList),
+    ingredients: ingredientList,
+    instructions: instructionList,
+  };
 
-    //  selecting which which contentType to convert to
-    var convertedData = dataToType(obtainedData,selectedFileType)
-
-
-    var filename = stringToFilename(obtainedData["name"])
-    
-    downloadRecipe(convertedData,filename,filePrefix,selectedFileType)
-    
+  return condensedInformation;
 }
 
-function dataToType(unconvertedData,selectedFileType){
-    // function selecting which format to convert to
-    // returns the data afterwards
-    switch(selectedFileType){
-        case "markdown":
-            console.log("choosen markdown")
-
-            return convertToMarkdown(unconvertedData)
-        case "json":
-            console.log("choosen json")
-            return convertToJson(unconvertedData)
-        default:
-            return convertToMarkdown(unconvertedData)
-    }
+function gatherIngredients() {
+  // anchor to ul-element
+  var ingredientDomAnchor = document.getElementsByClassName(
+    "wprm-recipe-ingredients"
+  )[0];
+  var ingredientList = traverseList(ingredientDomAnchor, (item) => {
+    return item.innerText.replace("▢\n", "");
+  });
+  return ingredientList;
 }
 
+function gatherPortionAmount() {
+  var portionAmountAnchor =
+    document.getElementsByClassName("wprm-toggle-active")[0];
+  var portionAmount = portionAmountAnchor.innerText;
 
-function downloadRecipe(convertedFileContent,name,filePrefix,type){
-    // downloading Recipe after conversion
-    console.log("downloading converted recipe")
-
-    var fileEnding = determineFileEnding(type)
-    var filename = `${filePrefix}${type}_${name}${fileEnding}`
-
-    var downloadLink = document.createElement('a');
-    downloadLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(convertedFileContent));
-    downloadLink.setAttribute("download",filename);
-    
-    downloadLink.style.display = 'none';
-    
-    // document.body.appendChild(downloadLink);
-
-    downloadLink.click();
-
-    // document.body.downloadLink(downloadLink);
+  //  convert from 1x to 1
+  convertedPortionAmount = portionAmount.replace("x", "");
+  return convertedPortionAmount;
 }
 
-function stringToFilename(string){
-    // converts string to be filename friendly. 
-    //  this means removeing escape characters and whitespaces
-    // var regexDisallowedCharacters = /\\'/
-    convertedString = string.replaceAll(" ","-")
-    return convertedString
+function gatherInstructions() {
+  // anchor to ul-element of instructions
+  var instructionDomAnchor = document.getElementsByClassName(
+    "wprm-recipe-instructions"
+  )[0];
+  var instructionList = traverseList(instructionDomAnchor, prepareContent);
+  return instructionList;
 }
 
-
-function determineFileEnding(filetype){
-    // TODO convert to matching Case
-    switch(filetype){
-        case "markdown":
-            return ".md"
-        case "json":
-            return ".json"
-        default:
-            return ".txt"
-    }
+function gatherDescription() {
+  var descriptionDomAnchor = document.getElementsByClassName(
+    "wprm-recipe-summary"
+  )[0];
+  // var descriptionList = traverseList(descriptionDomAnchor,prepareContent)
+  var descriptionList = traverseList(descriptionDomAnchor, processText);
+  return descriptionList;
 }
 
-function convertToMarkdown(condensedRecipe){
-    // generating markdown string to download as file
+function gatherImage() {
+  // queries for given class and returns href of image
+  var imageDomAnchor = document.getElementsByClassName("wprm-recipe-image")[0];
+  var imageChildNode = imageDomAnchor.childNodes[0];
+  var imageSource = imageChildNode.src;
 
-    var ingredientList = condensedRecipe["ingredients"]
-    ingredientListMarkdown = ArrayToMarkdownString(ingredientList)
-    var descriptionList = condensedRecipe["instructions"]
-    descriptionListMarkdown = ArrayToMarkdownString(descriptionList)
-
-    var portionAmount = condensedRecipe["portion"]
-
-    var markdownString = 
-    `# ${condensedRecipe["name"]}: \n 
-anchored to **EnterNode** 
-
-original Recipe found [here](${condensedRecipe["url"]})
-
---- 
-## Description:
-${condensedRecipe["description"]}
-
-This recipe will yield ${portionAmount} portions
-
-## Ingredients:
-This recipe requires the following **ingredients** for ${portionAmount} portions
-${ingredientListMarkdown}
-    
-## Instructions:
-Follow belows set of instruction to prepare this **meal**. 
-${descriptionListMarkdown}
-
-### Image:
-This image was provided by the original website hosting it.
-
-![image](${condensedRecipe["imageSource"]})
-
---- 
-Enjoy your meal. 
-
-    ` 
-    return markdownString
-
+  return imageSource;
 }
 
-function convertToJson(condensedRecipe){
-    console.log("converting to json")
-    // given a struct with:
-    // @param condensedRecipe
-    // - name 
-    // - description
-    // - ingredients
-    // - instructions
-    // returns Json
-    var recipeName = condensedRecipe["name"]
-    var recipeId = hashCode(recipeName)
-    var jsonContent = {
-    "id": Math.abs(recipeId),
-    "name": recipeName,
-    "description": condensedRecipe["description"],
-    "url":condensedRecipe["url"],
-    "image":condensedRecipe["imageSource"],
-    "prepTime":null,
-    "cookTime":null,
-    "totalTime":null,
-    "recipeCategory":"",
-    "keywords":"",
-    "recipeYield":condensedRecipe["portion"],
-    "tool":[],
-    "recipeIngredient":condensedRecipe["ingredients"],
-    "recipeInstructions":condensedRecipe["instructions"],
-    "nutrition":{},
-    "@context":"http:\/\/schema.org",
-    "@type":"Recipe",
-    "dateModified":"2023-09-29T23:34:10+0000",
-    "dateCreated":"2023-09-29T23:34:10+0000",
-    }
-    var convertedJson = JSON.stringify(jsonContent)
-
-    // convertedJson = jsonToReadableJson(convertedJson)
-    return convertedJson
-    // console.log("converting to json")
-    // var jsonRepresentation = JSON.stringify(condensedRecipe)
-    // console.log(jsonRepresentation)
-}
-
-function gatherContent(){
-    console.log("gathering information")
-    var recipeName = document.getElementsByClassName("wprm-recipe-name")[0].innerHTML
-
-    var descriptionList = gatherDescription();
-    var ingredientList = gatherIngredients()
-    var instructionList = gatherInstructions();
-    var recipeLink = window.location.href
-    var portionAmount = gatherPortionAmount();
-    var imageSource = gatherImage();
-
-    condensedInformation = { 
-        'name' : recipeName,
-        'url': recipeLink,
-        'portion' : portionAmount,
-        'imageSource': imageSource,
-        'description' : summarizeArray(descriptionList),
-        'ingredients' : ingredientList,
-        'instructions' : instructionList,
-    }
-
-    return condensedInformation
-}
-
-function gatherIngredients(){
-    // anchor to ul-element
-    var ingredientDomAnchor = document.getElementsByClassName("wprm-recipe-ingredients")[0]
-    var ingredientList = traverseList(ingredientDomAnchor,(item) => { return item.innerText.replace("▢\n","")})
-    return ingredientList
-}
-
-function gatherPortionAmount(){
-    var portionAmountAnchor = document.getElementsByClassName("wprm-toggle-active")[0];
-    var portionAmount = portionAmountAnchor.innerText;
-
-    //  convert from 1x to 1
-    convertedPortionAmount = portionAmount.replace("x","")
-    return convertedPortionAmount
-}
-
-function gatherInstructions(){
-    // anchor to ul-element of instructions 
-    var instructionDomAnchor = document.getElementsByClassName("wprm-recipe-instructions")[0]
-    var instructionList = traverseList(instructionDomAnchor,prepareContent)
-    return instructionList
-}
-
-function gatherDescription(){
-    var descriptionDomAnchor = document.getElementsByClassName("wprm-recipe-summary")[0]
-    // var descriptionList = traverseList(descriptionDomAnchor,prepareContent)
-    var descriptionList = traverseList(descriptionDomAnchor,processText)
-    return descriptionList
-}
-
-function gatherImage(){
-    // queries for given class and returns href of image 
-    var imageDomAnchor = document.getElementsByClassName("wprm-recipe-image")[0]
-    var imageChildNode = imageDomAnchor.childNodes[0]
-    var imageSource = imageChildNode.src
-
-    return imageSource
-}
-
-// general function traversing through a list of given elements 
+// general function traversing through a list of given elements
 // extracts each ChildNode's content according to supplied "prepareContent" function
-function traverseList(topListElement,functionToCall){
-    var obtainedElements = []
-    var ListChildNodes = topListElement.childNodes 
-    ListChildNodes.forEach( (childNode) => {
-        // var processedContent = prepareContent(childNode)
-        var processedContent = functionToCall(childNode)
-        obtainedElements.push( processedContent)
-    })
-    return obtainedElements
+function traverseList(topListElement, functionToCall) {
+  var obtainedElements = [];
+  var ListChildNodes = topListElement.childNodes;
+  ListChildNodes.forEach((childNode) => {
+    // var processedContent = prepareContent(childNode)
+    var processedContent = functionToCall(childNode);
+    obtainedElements.push(processedContent);
+  });
+  return obtainedElements;
 }
 
 // used to process Data obtained within a Node
-function prepareContent(contentNode){
-    return contentNode.innerText
+function prepareContent(contentNode) {
+  return contentNode.innerText;
 }
 
-function processSpecialElement(childNode){
-    if(childNode.localName == "a"){
-        var convertedLink = `[${childNode.innerHTML}](${childNode.href})`;
-        return convertedLink
-    } else if (childNode.localName == "em"){
-        var convertedUnderline = `_${childNode.innerText}_`;
-        return convertedUnderline
-    } else {
-        return childNode.innerText;
+function processSpecialElement(childNode) {
+  if (childNode.localName == "a") {
+    var convertedLink = `[${childNode.innerHTML}](${childNode.href})`;
+    return convertedLink;
+  } else if (childNode.localName == "em") {
+    var convertedUnderline = `_${childNode.innerText}_`;
+    return convertedUnderline;
+  } else {
+    return childNode.innerText;
+  }
+}
 
+function processText(contentNode) {
+  // supposed this Node includes further nodes with more content
+  // possible types are:
+  // - Links
+  // - list
+  // - empty
+  var NodeChildLength = contentNode.childNodes.length;
+  if (NodeChildLength == 1) {
+    // found special case -> link, underlined or similar
+    return processSpecialElement(contentNode);
+  } else if (NodeChildLength > 0) {
+    // found item with multiple items within, traversing again
+    nestedContentList = [];
+    contentNode.childNodes.forEach((childNodeFound) => {
+      var processedContent = processText(childNodeFound, processText);
+      nestedContentList.push(processedContent);
+    });
+    return nestedContentList;
+  } else {
+    // empty child
+    var gatheredContent = contentNode.innerText;
+    if (gatheredContent == undefined) {
+      gatheredContent = contentNode.nodeValue;
     }
+    return gatheredContent;
+  }
 }
 
-function processText(contentNode){
-    // supposed this Node includes further nodes with more content 
-    // possible types are:
-    // - Links 
-    // - list 
-    // - empty 
-    var NodeChildLength = contentNode.childNodes.length
-    if (NodeChildLength == 1){
-        // found special case -> link, underlined or similar
-        return processSpecialElement(contentNode)
-    } else if (NodeChildLength > 0){
-        // found item with multiple items within, traversing again
-        nestedContentList = []
-        contentNode.childNodes.forEach( (childNodeFound) =>{
-            var processedContent = processText(childNodeFound,processText)
-            nestedContentList.push(processedContent)
-
-        })
-        return nestedContentList
-    } else {
-        // empty child
-        var gatheredContent = contentNode.innerText
-        if (gatheredContent == undefined){
-            gatheredContent = contentNode.nodeValue
-        }
-        return gatheredContent
-    }
+function summarizeArray(ArrayToCompress) {
+  // converts array to string
+  var condensedString = "";
+  ArrayToCompress.forEach((item) => {
+    condensedString = condensedString + item;
+  });
+  return condensedString;
 }
-
-function ArrayToMarkdownString(ArrayToConvert){
-    // converts array to markdown string
-    var markdownListing = ""
-    for (i = 0 ; i < ArrayToConvert.length ; i++){
-        var listItem = `${i+1}. ${ArrayToConvert[i]}\n`
-        markdownListing += listItem
-    }
-    return markdownListing
-
-}
-
-
-function summarizeArray(ArrayToCompress){
-    // converts array to string 
-    var condensedString = ""
-    ArrayToCompress.forEach( (item) =>{
-        condensedString = condensedString + item
-
-    })
-    return condensedString
-}
-
 
 // taken from https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
 function hashCode(str) {
-    
-    return str.split('').reduce((prevHash, currVal) =>
-      (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
-  }
+  return str
+    .split("")
+    .reduce(
+      (prevHash, currVal) =>
+        ((prevHash << 5) - prevHash + currVal.charCodeAt(0)) | 0,
+      0
+    );
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,97 +1,282 @@
 // This file contains the primary code to extract
 // and prepare a recipe for download
 
-// global variables - unfortunately :/ 
-var statusCodeField = null
-var statusCodeText = "statuscode:"
-var debugField = null
-var currentTab = null
-
+// global variables - unfortunately :/
+var statusCodeField = null;
+var statusCodeText = "statuscode:";
+var debugField = null;
+var currentTab = null;
 
 // initial setup
-document.addEventListener("DOMContentLoaded",initializeScript)
-
+document.addEventListener("DOMContentLoaded", initializeScript);
 
 // function setting up environment for downloading
-function initializeScript(){
+function initializeScript() {
+  console.log("initialized script");
+  var downloadButton = document.getElementById("downloadRecipe");
+  statusCodeField = document.getElementById("statuscode");
+  debugField = document.getElementById("debug");
 
-    console.log("initialized script")
-    var downloadButton = document.getElementById("downloadRecipe");
-    statusCodeField = document.getElementById("statuscode");
-    debugField = document.getElementById("debug");
-
-    
-    downloadButton.addEventListener("click",setupDownload);
-    
-    
-
-
+  downloadButton.addEventListener("click", setupDownload);
 }
 
-function updateStatusCodeField(message){
-    statusCodeField.innerHTML = statusCodeText + message
+function updateStatusCodeField(message) {
+  statusCodeField.innerHTML = statusCodeText + message;
 }
-function writeDebug(message){
-    debugField.innerHTML = message
-    debugField.style="display:none"
+function writeDebug(message) {
+  debugField.innerHTML = message;
+  debugField.style = "display:none";
 }
 
-// function gathers the first tab from a list 
-// of tabs 
+// function gathers the first tab from a list
+// of tabs
 // sets this tabs as "currentTab"
 async function gatherActiveTab() {
-    activeTab = await browser.tabs.query({currentWindow:true, active:true,}).then( (result) => { 
+  activeTab = await browser.tabs
+    .query({ currentWindow: true, active: true })
+    .then(
+      (result) => {
         // success
-            let tab = result[0]
-            currentTab = tab
-            updateStatusCodeField(currentTab.id)
-        },
-        // fails
-        console.error)
+        let tab = result[0];
+        currentTab = tab;
+        updateStatusCodeField(currentTab.id);
+        return currentTab;
+      },
+      // fails
+      console.error
+    );
 }
 
-function gatherUserSelection(){
-    // function to select inputs from user 
-    var filePrefix = document.getElementById("filePrefix").value;
-    var selectedFileType = document.querySelector('input[name="filetype"]:checked').value;
+function gatherUserSelection() {
+  // function to select inputs from user
+  var filePrefix = document.getElementById("filePrefix").value;
+  var selectedFileType = document.querySelector(
+    'input[name="filetype"]:checked'
+  ).value;
 
-    var gatheredInput = {
-        filePrefix: filePrefix,
-        fileType:selectedFileType,
-    }
-    return gatheredInput
+  var gatheredInput = {
+    filePrefix: filePrefix,
+    fileType: selectedFileType,
+  };
+  return gatheredInput;
 }
 
 async function setupDownload() {
-    updateStatusCodeField("button clicked");
-    // detecting active Tab
-    var userInput =gatherUserSelection();
-    await gatherActiveTab().then( () => {
+  updateStatusCodeField("button clicked");
+  // detecting active Tab
+  var userInput = gatherUserSelection();
+  var activeTab = await gatherActiveTab();
+  // writeDebug(currentTab.id);
+  executeScript(currentTab, userInput);
+}
 
+async function executeScript(activeTab, userSelection) {
+  // executing content script to gather recipe
 
-    })
-    // writeDebug(currentTab.id);
+  // FIXME query how to crawl through the website
+  // after all getting the information depends on the website observed
+  // allows to separate those into files
+  var crawlerPath = selectCrawler("nutritionfacts");
 
-    // sending 
-    try {
-        await browser.scripting.executeScript({
-            target:{
-                tabId: currentTab.id,
-            },
-            files: ["/javascript/contentScript_nutritionfacts.js"],
-        }).then( () => { 
+  try {
+    await browser.scripting
+      .executeScript({
+        target: {
+          tabId: activeTab.id,
+        },
 
-            // sending acquired parameters to **content script**
-            const parameter = browser.tabs.sendMessage(
-                currentTab.id,
-                userInput
-            )
+        files: crawlerPath,
+      })
+      .then(() => {
+        // sending acquired parameters to **content script**
+        const parameter = browser.tabs
+          .sendMessage(currentTab.id, userSelection)
+          .then((response) => {
+            // gathering response from tab
+            // converting obtained recipe to requested format
+            var convertedData = dataToType(
+              response.recipe,
+              userSelection.fileType
+            );
+            // downloading recipe
+            var recipeName = stringToFilename(response.recipe.name);
+            downloadRecipe(
+              convertedData,
+              recipeName,
+              userSelection.filePrefix,
+              userSelection.fileType
+            );
+          });
+      });
+  } catch (err) {
+    updateStatusCodeField(`failed to execute script ${err}`);
+  }
+}
 
-        })
-    } catch (err) { 
-        updateStatusCodeField(`failed to execute script ${err}`)
-    }
+function selectCrawler(usedWebsite) {
+  // function selecting which crawler to use
+  // returns the crawler afterwards
+  switch (usedWebsite) {
+    case "nutritionfacts":
+      return ["/javascript/contentScript_nutritionfacts.js"];
+  }
+}
 
+/*
+case "https://www.loveandlemons.com/":
+case "https://www.skinnytaste.com/":
+*/
+
+// ----------------- conversion of retrieved recipe -----------------
+
+function dataToType(unconvertedData, selectedFileType) {
+  // function selecting which format to convert to
+  // returns the data afterwards
+  switch (selectedFileType) {
+    case "markdown":
+      updateStatusCodeField("choosen markdown");
+
+      return convertToMarkdown(unconvertedData);
+    case "json":
+      updateStatusCodeField("choosen json");
+      return convertToJson(unconvertedData);
+    default:
+      return convertToMarkdown(unconvertedData);
+  }
+}
+
+function convertToMarkdown(condensedRecipe) {
+  // generating markdown string to download as file
+
+  var ingredientList = condensedRecipe["ingredients"];
+  ingredientListMarkdown = ArrayToMarkdownString(ingredientList);
+  var descriptionList = condensedRecipe["instructions"];
+  descriptionListMarkdown = ArrayToMarkdownString(descriptionList);
+
+  var portionAmount = condensedRecipe["portion"];
+
+  var markdownString = `# ${condensedRecipe["name"]}: \n 
+anchored to **EnterNode** 
+
+original Recipe found [here](${condensedRecipe["url"]})
+
+--- 
+## Description:
+${condensedRecipe["description"]}
+
+This recipe will yield ${portionAmount} portions
+
+## Ingredients:
+This recipe requires the following **ingredients** for ${portionAmount} portions
+${ingredientListMarkdown}
     
-    
+## Instructions:
+Follow belows set of instruction to prepare this **meal**. 
+${descriptionListMarkdown}
+
+### Image:
+This image was provided by the original website hosting it.
+
+![image](${condensedRecipe["imageSource"]})
+
+--- 
+Enjoy your meal. 
+
+    `;
+  return markdownString;
+}
+
+function ArrayToMarkdownString(ArrayToConvert) {
+  // converts array to markdown string
+  var markdownListing = "";
+  for (i = 0; i < ArrayToConvert.length; i++) {
+    var listItem = `${i + 1}. ${ArrayToConvert[i]}\n`;
+    markdownListing += listItem;
+  }
+  return markdownListing;
+}
+
+function convertToJson(condensedRecipe) {
+  console.log("converting to json");
+  // given a struct with:
+  // @param condensedRecipe
+  // - name
+  // - description
+  // - ingredients
+  // - instructions
+  // returns Json
+  var recipeName = condensedRecipe["name"];
+  var recipeId = hashCode(recipeName);
+  var jsonContent = {
+    id: Math.abs(recipeId),
+    name: recipeName,
+    description: condensedRecipe["description"],
+    url: condensedRecipe["url"],
+    image: condensedRecipe["imageSource"],
+    prepTime: null,
+    cookTime: null,
+    totalTime: null,
+    recipeCategory: "",
+    keywords: "",
+    recipeYield: condensedRecipe["portion"],
+    tool: [],
+    recipeIngredient: condensedRecipe["ingredients"],
+    recipeInstructions: condensedRecipe["instructions"],
+    nutrition: {},
+    "@context": "http://schema.org",
+    "@type": "Recipe",
+    dateModified: "2023-09-29T23:34:10+0000",
+    dateCreated: "2023-09-29T23:34:10+0000",
+  };
+  var convertedJson = JSON.stringify(jsonContent);
+
+  // convertedJson = jsonToReadableJson(convertedJson)
+  return convertedJson;
+  // console.log("converting to json")
+  // var jsonRepresentation = JSON.stringify(condensedRecipe)
+  // console.log(jsonRepresentation)
+}
+
+function determineFileEnding(filetype) {
+  // TODO convert to matching Case
+  switch (filetype) {
+    case "markdown":
+      return ".md";
+    case "json":
+      return ".json";
+    default:
+      return ".txt";
+  }
+}
+
+function stringToFilename(string) {
+  // converts string to be filename friendly.
+  //  this means removeing escape characters and whitespaces
+  // var regexDisallowedCharacters = /\\'/
+  convertedString = string.replaceAll(" ", "-");
+  return convertedString;
+}
+
+// downloading recipe to file
+function downloadRecipe(convertedFileContent, filename, filePrefix, type) {
+  // downloading Recipe after conversion
+  console.log("downloading converted recipe");
+
+  var fileEnding = determineFileEnding(type);
+  var recipeFile = `${filePrefix}${type}_${filename}${fileEnding}`;
+
+  var downloadLink = document.createElement("a");
+  downloadLink.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(convertedFileContent)
+  );
+  downloadLink.setAttribute("download", recipeFile);
+
+  downloadLink.style.display = "none";
+
+  // document.body.appendChild(downloadLink);
+
+  downloadLink.click();
+
+  // document.body.downloadLink(downloadLink);
 }
